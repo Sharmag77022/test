ca7619f5d060549af2385730d2b2003a0c197e08
Sharmag77022 <59397538+Sharmag77022@users.noreply.github.com>
2023-07-27 12:01:29 +0530

    change remotely in file1.js

--- file1.js
+++ file1.js
@@ -6,7 +6,7 @@ const Coupon = require("../models/coupon/couponModel.js");
 // const MongoClient = require('mongodb').MongoClient;
 //hello this is sanjeev testing
 
-const DB =
+const MB =
   process.env.NODE_ENV === "production"
     ? process.env.DB_URI_PROD.replace("<USERNAME>", process.env.MONGO_USERNAME)
         .replace("<PASSWORD>", process.env.MONGO_PASSWORD)
